64d5e1a807b76c77f334e3103bdcd67a4fa11cdd
First Last <FL@academind.com>
2023-01-29 23:15:34 -0500

    week 4

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Pets-R-Us: Home',
+        pageTitle: 'Landing Page'
+    })
+});
+
+app.get('/grooming', (req, res) => {
+    res.render('grooming', {
+        title: 'Pets-R-Us: Grooming',
+        pageTitle: 'Landing Page'
+    })
+});
+
+
+app.listen(3000, () => console.log("Running"));
